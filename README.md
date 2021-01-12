28785beb6981a24127feb18ee03a587a6cd0e6c7
Vikas <44545660+vikasdo@users.noreply.github.com>
2021-01-12 10:39:00 +0000

    add login

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Book-Recommendation-Analysis
+`
+
+1.Source env/bin/activate
+2 .pip install -r requirements.txt
+
+``
---
+++ bookstore/models.py
@@ -0,0 +1,25 @@
+from reviewcruncher import db
+from flask_login import UserMixin
+
+
+"""
+These are all the Models which we are currently using in our App.
+"""
+
+
+class User(UserMixin, db.Model):
+	__table_args__ = (
+        db.UniqueConstraint('email', 'id', name='unique_component_commit'),
+    )
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(255), nullable=False)
+	email = db.Column(db.String(100), nullable=False)
+	
+	password = db.Column(db.String(255), nullable=False)
+	session_token = db.Column(db.String(255), index=True)
+
+	def get_id(self):
+		return str(self.session_token)
+
+	def __repr__(self):
+		return f"User('{self.id}','{self.name}','{self.email}','{self.company}')"
