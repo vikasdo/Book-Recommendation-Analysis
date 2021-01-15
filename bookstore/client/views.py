88d421dc0a1141e9467ebf4a61ae836abaa78760
VamsiMudaliar <vamsimudaliar@gmail.com>
2021-01-15 21:10:50 +0530

    Check now

--- bookstore/client/views.py
+++ bookstore/client/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:209f5c571500c4d72898c53e9dc78eddc5471c77d234ac93d3bac2cb79c68208
-size 3381
+oid sha256:6d8fc2af0fa1730f405282fd051fba26f3b59cd00d36c73ea88528a87bed7549
+size 3563
--- bookstore/static/helper.js
+++ bookstore/static/helper.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c9f13c15b5de57b1d443345250117c3fb1a43e94b2c4e67a6408d69ce989111
-size 3070
+oid sha256:ed644ca9878592b4f5317b456a6e1fa0d582173c7019c706f6b5c04ca8b29a06
+size 3462
