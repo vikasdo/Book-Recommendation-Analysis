c1d8b808e2410d631d4ed1463c06343293365518
VamsiMudaliar <vamsimudaliar@gmail.com>
2021-01-15 17:45:05 +0530

    Added checkout feature

--- bookstore/client/views.py
+++ bookstore/client/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60e8454ecc6251a8108c74927a6545ef9c44b3d189b6f4cccfbb9cf24d04821f
-size 3229
+oid sha256:209f5c571500c4d72898c53e9dc78eddc5471c77d234ac93d3bac2cb79c68208
+size 3381
