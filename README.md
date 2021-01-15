5ff1b2aaee3f4718eadc0d85fb1cf433b6f96eff
VamsiMudaliar <vamsimudaliar@gmail.com>
2021-01-15 17:45:05 +0530

    Added checkout feature

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce00488376d6eca1dc776a3f9bd16bd39642e3b90de15a87244b2ade6be94485
-size 749
+oid sha256:b4d07255daa18852d5f689c9ccb9de5a96018a8f1bc5e23d4c0b10a836bc9678
+size 743
--- bookstore/static/helper.js
+++ bookstore/static/helper.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb99b0ce3cf0c17f96442ff5c1c984953a2888aa238022aee58e84eb245b94ca
-size 2052
+oid sha256:0c9f13c15b5de57b1d443345250117c3fb1a43e94b2c4e67a6408d69ce989111
+size 3070
