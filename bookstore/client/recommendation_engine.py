2d38dc0a7a273feee7ce1dbbebea17d56b8237ed
vikasdo <8466vikas@gmail.com>
2021-01-15 23:18:38 +0530

    add

--- bookstore/client/recommendation_engine.py
+++ bookstore/client/recommendation_engine.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90394ddb2f57cffbca1b1bb3e7f464ef8926e437299a346b7490567d66ca23c1
-size 6326
+oid sha256:64b3a1716f2839b9c6c527d5a499e252069bfd5f6a3af21d468821ef2dd81b10
+size 6366
--- bookstore/client/views.py
+++ bookstore/client/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d8fc2af0fa1730f405282fd051fba26f3b59cd00d36c73ea88528a87bed7549
-size 3563
+oid sha256:2434dbf5a686b2e278e991ccff74ba9b2f78921e8bf91b9f77f4dcc2578e3bd7
+size 4233
