0df0a7bca8e3ef1baaec993220ea16f2fcfaca81
VamsiMudaliar <vamsimudaliar@gmail.com>
2021-01-15 10:32:37 +0530

    Added Models

--- bookstore/client/views.py
+++ bookstore/client/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f1dc708968e185be36657eb83d64c18d2f1fd2b6840240f0be0a1b39631b5bf
-size 2261
+oid sha256:60e8454ecc6251a8108c74927a6545ef9c44b3d189b6f4cccfbb9cf24d04821f
+size 3229
