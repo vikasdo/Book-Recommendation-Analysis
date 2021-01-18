78807ca788afe5f75d1e7d25cea4253f80028d67
vikasdo <8466vikas@gmail.com>
2021-01-18 21:53:40 +0530

    add

--- bookstore/client/views.py
+++ bookstore/client/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3f8149d00048f0564f84afb2ee0f65473a43579e2654fd547933b86296b659c
-size 285
+oid sha256:55c540a7cb76c377d66ae9ea94b1a911efc2144db4b9fb52a24d57a0d6cc5f36
+size 4357
