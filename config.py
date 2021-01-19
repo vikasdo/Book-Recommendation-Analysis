8a107d7fc08d88795f172fbc61968670f10c6910
VamsiMudaliar <vamsimudaliar@gmail.com>
2021-01-19 17:28:56 +0530

    Recommended Books

--- config.py
+++ config.py
@@ -1,3 +1,44 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:179b81c9a379041a296245738129107ae5f36ea7737248212580a878a3c5bc1b
-size 1055
+import os
+# print(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..')))
+class BaseConfig(object):
+    """
+    Using this class to share any default attributes with any subsequent
+    classes that inherit from BaseConfig.
+    """
+    DEBUG = False
+
+ 
+    ENV="develpoment"
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = False
+
+ 
+
+
+    BASE_DIR = os.path.join( os.path.dirname(os.path.realpath(__file__)), 'bookstore')
+ 
+
+ 
+    PICKLES=os.path.join(BASE_DIR,'static','pickles')
+
+
+    # RETAIL_ANALYSIS=os.path.join(BASE_DIR,'client','utils','Retailer_Analysis.csv') 
+    BOOKPKL=os.path.join(PICKLES,'books.pkl') 
+    CACHEPKL=os.path.join(PICKLES,'cache.pkl') 
+    RATINGMATRIX=os.path.join(PICKLES,'rec_data.pkl') 
+
+
+
+class DevelopmentConfig(BaseConfig):
+
+    DEBUG = True
+    
+  
+
+
+    # For using SQLite3 databast
+    SQLITE_DB_DIR = os.path.join( os.path.dirname(os.path.realpath(__file__)), 'db.sqlite')
+    # print(SQLITE_DB_DIR)
+    SQLALCHEMY_DATABASE_URI = "sqlite:///"+SQLITE_DB_DIR
+    SQLALCHEMY_ECHO = False
--- manager.py
+++ manager.py
@@ -1,3 +1,4 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:85f7e45c40a84600775f6c644c4bce218b811cb4b2c9bcf31f7153fd3cd3a4c1
-size 72
+from bookstore import manager
+
+if __name__ == '__main__':
+	manager.run()
