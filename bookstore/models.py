39598e9ca130d2050d69ed74787576674fbecbb7
VamsiMudaliar <vamsimudaliar@gmail.com>
2021-01-20 12:01:30 +0530

    Fr

--- bookstore/models.py
+++ bookstore/models.py
@@ -14,9 +14,9 @@ class User(UserMixin, db.Model):
 	name = db.Column(db.String(255), nullable=False)
 	email = db.Column(db.String(100), nullable=False)
 	password = db.Column(db.String(255), nullable=False)
-
 	location = db.Column(db.String(255), nullable=False)
 	age = db.Column(db.String(255))
+	#user_rating = db.relationship('Ratings',backref="user_id",uselist=False)
 
 	def get_id(self):
 		return str(self.id)
@@ -26,16 +26,29 @@ class User(UserMixin, db.Model):
 
 class Books(db.Model):
 	bid = db.Column(db.Integer,primary_key=True)
-	ISBN = db.Column(db.String(10),nullable=False)
+	ISBN = db.Column(db.String(50),nullable=False)
 	title = db.Column(db.String(255),nullable=False)
 	author = db.Column(db.String(100),nullable=False)
 	publisher = db.Column(db.String(100),nullable=False)
 	book_cost = db.Column(db.String(100),nullable=False)
 	pubDate = db.Column(db.String(20),nullable=False,default="2001")
 	bookImage = db.Column(db.String(500),nullable=False)
+	#book_rating = db.relationship('Ratings',backref="book_id",uselist=False)
 
 	def __repr__(self):
 		return f"Books('{self.ISBN}','{self.title}','{self.bookImage}','{self.book_cost}')"
+
+class Ratings(db.Model):
+	rid = db.Column(db.Integer,primary_key=True)
+	rating = db.Column(db.Integer,default=0) 
+	user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+	book_id = db.Column(db.Integer,db.ForeignKey('books.bid'))
+
+
+	def __repr__(self):
+		return f"Ratings('{self.book_id}','{self.user_id}','{self.rating}')"
+
+
 '''
 class Cart(UserMixin,db.Model):
 	cart_id = db.Column(db.Integer,primary_key=True)
