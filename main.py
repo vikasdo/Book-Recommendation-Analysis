d807f1784a0b2fcd6336c87f1d852ac8b78a8e79
vikasdo <donthula.vikas@koireader.com>
2021-03-16 17:55:22 +0530

    'ad'

--- main.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from bookstore import app
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
--- manager.py
+++
@@ -1,4 +0,0 @@
-from bookstore import manager
-
-if __name__ == '__main__':
-	manager.run()
