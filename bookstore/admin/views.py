933cdfb7b2a649f856e2e05daaba98fc285345d4
Vishvam Porwal <vishvamporwal1@gmail.com>
2021-05-13 14:15:02 +0530

    contact reply feature and some ui changes

--- bookstore/admin/views.py
+++ bookstore/admin/views.py
@@ -5,17 +5,26 @@ from bookstore.admin.admin_credentials import admin_password, admin_username
 from functools import wraps
 import datetime
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
 from bookstore.admin import admin_functions
 
 
 admin = Blueprint('admin', __name__)
 
 
+#flask mail config
+app.config["MAIL_DEFAULT_SENDER"] = "bookly1120@gmail.com"
+app.config["MAIL_PASSWORD"] = "bookly@112000"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = "bookly1120"
+mail = Mail(app)
 
 
 ##custom decorators
-#check if admin is logged in or not based on session
-def login_required(f):
+#check if admin is logged in or not based on session variable2
+def admin_login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if 'admin_in' not in session:
@@ -27,7 +36,7 @@ def login_required(f):
 ##routes
 #index of admin
 @app.route('/admin/')
-@login_required
+@admin_login_required
 def admin_index():
     year = datetime.datetime.now().year
     sales_yearly = admin_functions.calc_sales(year)
@@ -37,7 +46,7 @@ def admin_index():
 
 #add book
 @app.route('/admin/addbook', methods = ['GET', 'POST'])
-@login_required
+@admin_login_required
 def admin_add_book():
     if request.method == 'GET':
         return render_template('admin/add_book.html')
@@ -74,9 +83,45 @@ def admin_login():
             return redirect(url_for('admin_login'))
 
 
+#contact request
+@app.route('/admin/contact_request', methods = ['POST', 'GET'])
+@admin_login_required
+def admin_contact_request():
+    if request.method == 'POST':
+        if request.form.get('reply'):
+            cid = request.form.get('reply')
+            contact_to_reply = Contact.query.filter_by(cid = cid).first()
+            session['reply_email'] = contact_to_reply.email
+            return render_template('admin/reply_contact_request.html', contact = contact_to_reply)
+        else:
+            cid = request.form.get('delete')
+            contact_to_be_del = Contact.query.filter_by(cid = cid).first()
+            db.session.delete(contact_to_be_del)
+            db.session.commit()
+            return redirect(url_for('admin_contact_request'))
+    to_contact = Contact.query.filter_by().all()
+    return render_template('admin/contact_request.html', to_contact = to_contact)
+
+
+#reply via email
+@app.route('/admin/contact_reply', methods = ['POST'])
+@admin_login_required
+def admin_contact_reply():
+    reply_message = request.form.get('reply_message')
+    try:
+        message = Message("Reply to Contact query from Bookly!!",
+                        sender='bookly1120@gmail.com', recipients=[session['reply_email']])
+        message.body = reply_message
+        mail.send(message)
+        flash(f'Email sent.', 'success')
+    except:
+        flash(f'Error sending email.', 'error')
+    return redirect(url_for('admin_contact_request'))
+
+
 # admin logout
 @app.route('/admin/logout')
-@login_required
+@admin_login_required
 def admin_logout():
     session.pop('admin_in', None)
     flash('logged out', category='info')
